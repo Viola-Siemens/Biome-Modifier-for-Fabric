41af761f333a13978e80413c973f23a44478c150
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-01-20 22:11:58 +0800

    Compat with Fabric API

---
+++ src/main/java/com/hexagram2021/biome_modifier/mixin/fabric_api/BiomeModificationImplMixin.java
@@ -0,0 +1,23 @@
+package com.hexagram2021.biome_modifier.mixin.fabric_api;
+
+import net.fabricmc.fabric.impl.biome.modification.BiomeModificationImpl;
+import net.minecraft.world.level.dimension.LevelStem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings({"UnstableApiUsage", "java:S100", "java:S1118"})
+@Mixin(value = BiomeModificationImpl.class, remap = false)
+public class BiomeModificationImplMixin {
+	/**
+	 * Skip memoize
+	 * @param dimensionOptions dimension options
+	 * @param ci callback info
+	 * @see com.hexagram2021.biome_modifier.mixin.ChunkGeneratorMixin#biome_modifier$redirectMemoize
+	 */
+	@Inject(method = "lambda$finalizeWorldGen$6", at = @At(value = "HEAD"), cancellable = true)
+	private static void biome_modifier$redirectMemoize(LevelStem dimensionOptions, CallbackInfo ci) {
+		ci.cancel();
+	}
+}
